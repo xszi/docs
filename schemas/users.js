a21dad3041f5bb46f51c06456be8346d45431cf2
xszi <jiuhuaw@126.com>
2018-06-25 07:43:47 +0800

    my blog
    my blog

---
+++ schemas/users.js
@@ -0,0 +1,16 @@
+
+var mongoose=require('mongoose');
+
+//用户的表结构
+module.exports=new mongoose.Schema({
+
+    //用户名
+    username:String,
+    // 密码
+    password:String,
+    //管理员用户
+    isAdmin:{
+        type:Boolean,
+        default:false
+    }
+});
