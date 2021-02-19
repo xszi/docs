da57d9725f8efa39546babddce89d059c4be08d2
xszi <jiuhuaw@126.com>
2021-02-19 10:54:17 +0800

    no commit

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -12,7 +12,7 @@ module.exports = {
             },
             {
                 text: '机器学习',
-                link: 'https://github.com/xszi/javascript-algorithms'
+                link: 'https://xszi.github.io/blog/'
             },
             {
                 text: '感悟杂记',
@@ -24,34 +24,33 @@ module.exports = {
             },
         ],
         sidebar: [{
-                title: '2021训练专题',
-                collapsable: false,
-                children: [
-                    'topic/algorithms',
-                    'topic/webpack',
-                    'topic/monitor'
-                ]
-            }, {
-                title: 'TypeScript',
-                collapsable: false,
-                children: [
-                    'typescript/intro',
-                    'typescript/install',
-                    'typescript/start'
-                ]
-            }
-        ]
+            title: '2021训练专题',
+            collapsable: false,
+            children: [
+                'topic/algorithms',
+                'topic/webpack',
+                'topic/monitor'
+            ]
+        }, {
+            title: 'TypeScript',
+            collapsable: false,
+            children: [
+                'typescript/intro',
+                'typescript/install',
+                'typescript/start'
+            ]
+        }]
     },
     plugins: {
         '@vssue/vuepress-plugin-vssue': {
-          platform: 'github', //v3的platform是github，v4的是github-v4
-          locale: 'zh', //语言
-          // 其他的 Vssue 配置
-          owner: 'xszi', //github账户名
-          repo: 'blog', //github一个项目的名称
-          clientId: '46b28c3bacd5c436fe28',//注册的Client ID
-          clientSecret: '87fb68373df686731ab6578bb3eb56f48269c117',//注册的Client Secret
-          autoCreateIssue:true // 自动创建评论，默认是false，最好开启，这样首次进入页面的时候就不用去点击创建评论的按钮了。
+            platform: 'github', //v3的platform是github，v4的是github-v4
+            locale: 'zh', //语言
+            // 其他的 Vssue 配置
+            owner: 'xszi', //github账户名
+            repo: 'xszi.github.io', //github一个项目的名称
+            clientId: '46b28c3bacd5c436fe28', //注册的Client ID
+            clientSecret: '87fb68373df686731ab6578bb3eb56f48269c117', //注册的Client Secret
+            autoCreateIssue: true // 自动创建评论，默认是false，最好开启，这样首次进入页面的时候就不用去点击创建评论的按钮了。
         }
     }
 }
--- docs/README.md
+++ docs/README.md
@@ -4,3 +4,5 @@ home: true
 actionText: 开始学习呀 →
 actionLink: /chapter1/
 ---
+
+<Vssue title="我是一个Issue" />
